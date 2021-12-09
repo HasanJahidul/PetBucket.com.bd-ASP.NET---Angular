1cd9eed444386505175f413536167c56d5b7d90e
hasanjahidul <jahidul0hasan@gmail.com>
2021-12-10 02:53:24 +0600

    deleting angular app

--- ASP.NET-Backend/ASP.NET-Backend/Controllers/PetServiceController.cs
+++ ASP.NET-Backend/ASP.NET-Backend/Controllers/PetServiceController.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.IO;
+using System.Collections.Specialized;
+using System.Web;
 
 namespace ASP.NET_Backend.Controllers
 {
@@ -30,6 +33,26 @@ namespace ASP.NET_Backend.Controllers
         {
             PetService.AddRequest(request);
         }
+        /*[Route("api/sms")]
+        [HttpGet]
+        public string SMS()
+         {
+             string apikey = "NmI2MjM3NzY0OTZkNjM2ZTM0NzAzNTc2NGQzNTRhNTc=";
+
+             string SenderName = "PETBUCKET ";
+             string Number = "+8801631137535";
+             string Message = "This is an API message";
+             string URL = "https://api.txtlocal.com/send/?";
+             string PostData = "apikey=" + apikey + "&sender=" + SenderName + "&numbers=" + Number + "&message=" + Message;
+             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(URL);
+           
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                StreamReader sr = new StreamReader(resp.GetResponseStream());
+                string results = sr.ReadToEnd();
+            return results;
+            
+         }*/
+        
 
     }
 }
